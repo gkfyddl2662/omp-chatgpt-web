28e1e17826fc3aa5991352de9261bbed172ae8e5
SUMOREKO <smallrecorder12@gmail.com>
2026-09-23 07:05:51 +0900

    feat: export OMP runtime tool adapters

--- src/omp/index.ts
+++ src/omp/index.ts
@@ -3,3 +3,5 @@ export * from "./extension.js";
 export * from "./extension-manifest.js";
 export * from "./model-definition.js";
 export * from "./omp-model-registry.js";
+export * from "./registry-runtime.js";
+export * from "./tools/index.js";
