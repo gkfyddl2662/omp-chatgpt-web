9b810640bc1e137fd0e45a35d21a7997c442b263
SUMOREKO <smallrecorder12@gmail.com>
2026-09-23 06:55:28 +0900

    feat: export ChatGPT Web provider

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./backend-policy.js";
 export * from "./capability-broker.js";
 export * from "./chatgpt-web/index.js";
+export * from "./provider/chatgpt-web-provider.js";
 export * from "./types.js";
