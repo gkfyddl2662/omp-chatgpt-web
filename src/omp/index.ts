f02557f0dac1f3a3064ae680a399384dfb8161e9
SUMOREKO <smallrecorder12@gmail.com>
2026-09-23 06:58:31 +0900

    feat: export OMP model registry and manifest

--- src/omp/index.ts
+++ src/omp/index.ts
@@ -1,3 +1,5 @@
 export * from "./provider-adapter.js";
 export * from "./extension.js";
+export * from "./extension-manifest.js";
 export * from "./model-definition.js";
+export * from "./omp-model-registry.js";
